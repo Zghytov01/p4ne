2aec1d0b48048473f83f3070246586d365f8d514
ka.zhgutov <ka.zhgutov@jet.su>
2025-03-18 21:42:33 +0300

    add label and change some var

--- LABA 1.2/data_analysis.py
+++ LABA 1.2/data_analysis.py
@@ -4,14 +4,16 @@ from openpyxl import load_workbook
 wb=load_workbook("data_analysis_lab.xlsx")
 sheet=wb["Data"]
 ears = sheet['A'][1:]
-temperatures = sheet['D'][1:]
+solar_activity = sheet['D'][1:]
 relative_temperature = sheet['C'][1:]
 
 def getvalue(x): return x.value
 list_x=list(map(getvalue,ears))
-list_z=list(map(getvalue,temperatures))
+list_z=list(map(getvalue,solar_activity))
 list_y=list(map(getvalue,relative_temperature))
 pyplot.plot(list_x, list_y )
 pyplot.plot(list_x, list_z )
+pyplot.xlabel('Ears')
+pyplot.ylabel('Relative temperature')
 
 pyplot.show()
