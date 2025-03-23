7b336560afe63e71a82423d3dc0e70dda28b545f
ka.zhgutov <ka.zhgutov@jet.su>
2025-03-23 15:08:12 +0300

    new laba 1.3

--- LADA 1.5 log/Laba_1.5_log.py
+++ LADA 1.5 log/Laba_1.5_log.py
@@ -1,9 +1,13 @@
 import glob
-
+import re
+ip_list=[]
 for i in glob.glob('*.log'):
     with open(i) as j:
         list_j=j.readlines()
         for s in list_j:
             if "ip address" in s:
-
-                print(s)
+                ip_and_mask_str = " ".join(re.findall(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', s))
+                if ip_and_mask_str:
+                    ip_list.append(ip_and_mask_str)
+for ip in list(set(ip_list)):
+    print(ip)
